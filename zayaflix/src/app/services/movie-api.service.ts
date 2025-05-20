f0ee9c3f5e84e028efa2b091829697ad2d48f981
SuzaneChimenass <suzanecamila879@gmail.com>
2025-05-19 22:32:53 -0300

    Acrescentando carousel com dados da API

--- zayaflix/src/app/services/movie-api.service.ts
+++ zayaflix/src/app/services/movie-api.service.ts
@@ -9,5 +9,18 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
-  base
+  baseUrl = 'https://api.themoviedb.org/3';
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMjU3N2IxOGRhZWJlNWU2NzhhMmZkY2EzOTVmMGU5MiIsIm5iZiI6MTc0NzcwMDgwNC4wOSwic3ViIjoiNjgyYmNjNDRhNThjNTA5ODUzMmY3ODAzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.GMjj5FF5eP5kd1jRLrF4u4HxB940brkd0MkoKDjvWYU'
+    }
+  };
+
+  bannerApiData(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
+
 }
+
